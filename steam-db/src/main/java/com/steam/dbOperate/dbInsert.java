108e510650de06f981a29e7af429f47d7d8b70f1
Volleier <1206670314@qq.com>
2025-03-12 16:03:30 +0800

    change project version

--- steam-db/src/main/java/com/steam/dbOperate/dbInsert.java
+++ steam-db/src/main/java/com/steam/dbOperate/dbInsert.java
@@ -7,11 +7,26 @@ import java.sql.PreparedStatement;
 import com.steam.steamapi.pogo.GameList;
 
 public class dbInsert {
-    public static void insertGame(int gameAppid, String gameName, int gamePlaytime) {
-        String url = "jdbc:mysql://localhost:3306/game_db";
-        String user = "root";
-        String password = "root";
+    static String url = "jdbc:mysql://localhost:3306/game_db";
+    static String user = "root";
+    static String password = "root";
 
+    public static void insertGame(int gameAppid, String gameName, int gamePlaytime) {
+        insertSqlStatement(url, user, password, gameAppid, gameName, gamePlaytime);
+    }
+
+    public static void insertGame(com.steam.steamapi.pogo.GameList game) {
+
+
+        int gameAppid = game.getGameAppid();
+        String gameName = game.getGameName();
+        int gamePlaytime = game.getGamePlayTime();
+
+
+        insertSqlStatement(url, user, password, gameAppid, gameName, gamePlaytime);
+    }
+
+    private static void insertSqlStatement(String url, String user, String password, int gameAppid, String gameName, int gamePlaytime) {
         String sql = "INSERT INTO games (game_appid, game_name, game_playtime) VALUES (?, ?, ?)";
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
@@ -20,35 +35,6 @@ public class dbInsert {
             pstmt.setInt(1, gameAppid);
             pstmt.setString(2, gameName);
             pstmt.setInt(3, gamePlaytime);
-
-            int rowsInserted = pstmt.executeUpdate();
-            if (rowsInserted > 0) {
-                System.out.println("A new game was inserted successfully!");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void insertGame(com.steam.steamapi.pogo.GameList game) {
-        String url = "jdbc:mysql://localhost:3306/game_db";
-        String user = "root";
-        String password = "root";
-
-        int gameAppid = game.getGameAppid();
-        String gameName = game.getGameName();
-//        int gamePlaytime = game.getGamePlaytime();
-
-
-        String sql = "INSERT INTO games (game_appid, game_name, game_playtime) VALUES (?, ?, ?)";
-
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setInt(1, gameAppid);
-            pstmt.setString(2, gameName);
-//            pstmt.setInt(3, gamePlaytime);
 
             int rowsInserted = pstmt.executeUpdate();
             if (rowsInserted > 0) {
