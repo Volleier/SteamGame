a8d637f282159d0552bc69e11b1b488b9049bc14
Volleier <1206670314@qq.com>
2025-03-12 15:41:56 +0800

    Add files

---
+++ steam-api/src/main/java/com/steam/steamapi/SteamApiApplication.java
@@ -0,0 +1,13 @@
+package com.steam.steamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SteamApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SteamApiApplication.class, args);
+    }
+
+}
