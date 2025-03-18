883f23433f9bd7807095f9fec5980f7b0e1b126f
Volleier <1206670314@qq.com>
2025-03-18 16:27:39 +0800

    Initialize the Vue 3 project

---
+++ vue/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const msg = ref('Hello Vue 3 + Vite');
+import Apivue from './Api.vue';
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <br>
+  <Apivue />
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
