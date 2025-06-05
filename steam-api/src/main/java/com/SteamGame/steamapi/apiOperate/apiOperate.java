b0661cf8524bda43fb4d31f778f4c3cc180c074e
Volleier <1206670314@qq.com>
2025-06-05 17:14:52 +0800

    Refactors project structure and removes legacy code. Renaming the base package from `com.SteamGame.steamapi` to `com.SteamGame.api`.

--- steam-api/src/main/java/com/SteamGame/steamapi/apiOperate/apiOperate.java
+++
@@ -1,9 +0,0 @@
-package com.SteamGame.steamapi.apiOperate;
-
-import com.SteamGame.steamapi.pogo.GameList;
-
-import java.util.List;
-
-public interface apiOperate {
-    List<GameList> getGameList(String apiKey, String steamId);
-}
