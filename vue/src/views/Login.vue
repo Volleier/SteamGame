8f7dd3a68f0975f8498dc14f9cbb6495c5c7c1e6
Volleier <1206670314@qq.com>
2025-09-06 13:19:57 +0800

    Fade in and fade out

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -7,38 +7,40 @@
             </div>
             <div class="background-overlay"></div>
         </div>
-        <div class="login-card login-card-flex">
-            <div class="card-left">
-                <div class="icon">
-                    <img src="@/assets/images/Icon.png" alt="Icon" />
+        <transition name="card-fade" appear>
+            <div class="login-card login-card-flex">
+                <div class="card-left">
+                    <div class="icon">
+                        <img src="@/assets/images/Icon.png" alt="Icon" />
+                    </div>
+                </div>
+                <div class="card-divider"></div>
+                <div class="card-right">
+                    <div class="login-header">
+                        <h1>登录</h1>
+                        <p>使用你的Steam API来获取你的游戏信息</p>
+                    </div>
+                    <form class="login-form" @submit.prevent="handleLogin">
+                        <div class="form-group">
+                            <input id="Steam_Id" type="text" v-model="Steam_Id" disabled readonly
+                                class="form-input readonly-input" placeholder="暂无Steam ID数据" />
+                        </div>
+                        <div class="button-container">
+                            <button type="submit" class="login-button" :disabled="isLoading">
+                                <span v-if="isLoading" class="loading-spinner"></span>
+                                <span>{{ isLoading ? '登录中...' : '确认登录' }}</span>
+                            </button>
+                            <button type="button" class="register-button" @click="handleRegister">
+                                注册
+                            </button>
+                        </div>
+                        <div v-if="errorMessage" class="error-message">
+                            {{ errorMessage }}
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div class="card-divider"></div>
-            <div class="card-right">
-                <div class="login-header">
-                    <h1>登录</h1>
-                    <p>使用你的Steam API来获取你的游戏信息</p>
-                </div>
-                <form class="login-form" @submit.prevent="handleLogin">
-                    <div class="form-group">
-                        <input id="Steam_Id" type="text" v-model="Steam_Id" disabled readonly
-                            class="form-input readonly-input" placeholder="暂无Steam ID数据" />
-                    </div>
-                    <div class="button-container">
-                        <button type="submit" class="login-button" :disabled="isLoading">
-                            <span v-if="isLoading" class="loading-spinner"></span>
-                            <span>{{ isLoading ? '登录中...' : '确认登录' }}</span>
-                        </button>
-                        <button type="button" class="register-button" @click="handleRegister">
-                            注册
-                        </button>
-                    </div>
-                    <div v-if="errorMessage" class="error-message">
-                        {{ errorMessage }}
-                    </div>
-                </form>
-            </div>
-        </div>
+        </transition>
     </div>
 </template>
 
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -7,53 +7,55 @@
             </div>
             <div class="background-overlay"></div>
         </div>
-        <div class="register-card login-card-flex">
-            <div class="card-left">
-                <div class="icon">
-                    <img src="@/assets/images/Icon.png" alt="Icon" />
+        <transition name="card-fade" appear>
+            <div class="register-card login-card-flex">
+                <div class="card-left">
+                    <div class="icon">
+                        <img src="@/assets/images/Icon.png" alt="Icon" />
+                    </div>
+                </div>
+                <div class="card-divider"></div>
+                <div class="card-right">
+                    <div class="login-header">
+                        <h1>注册</h1>
+                    </div>
+
+                    <form class="login-form" @submit.prevent="handleRegister">
+                        <div class="form-group">
+                            <label for="register-steam-id" class="form-label">Steam ID</label>
+                            <input id="register-steam-id" type="text" v-model="registerSteamId" class="form-input"
+                                placeholder="请输入你的 Steam ID" required :disabled="isRegisterLoading" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="register-api-key" class="form-label">API Key</label>
+                            <input id="register-api-key" type="text" v-model="registerApiKey" class="form-input"
+                                placeholder="请输入你的 Steam API Key" required :disabled="isRegisterLoading" />
+                        </div>
+
+                        <div class="checkbox-container">
+                        </div>
+
+                        <div v-if="registerError" class="error-message">
+                            {{ registerError }}
+                        </div>
+
+                        <div class="button-container">
+                            <button type="submit" class="login-button" :disabled="isRegisterLoading">
+                                <span v-if="isRegisterLoading" class="loading-spinner"></span>
+                                <span>{{ isRegisterLoading ? '验证中...' : '提交' }}</span>
+                            </button>
+                        </div>
+                    </form>
+
+                    <transition name="toast">
+                        <div v-if="showRegisterSuccess" class="toast-message success">
+                            {{ registerSuccessMessage }}
+                        </div>
+                    </transition>
                 </div>
             </div>
-            <div class="card-divider"></div>
-            <div class="card-right">
-                <div class="login-header">
-                    <h1>注册</h1>
-                </div>
-
-                <form class="login-form" @submit.prevent="handleRegister">
-                    <div class="form-group">
-                        <label for="register-steam-id" class="form-label">Steam ID</label>
-                        <input id="register-steam-id" type="text" v-model="registerSteamId" class="form-input"
-                            placeholder="请输入你的 Steam ID" required :disabled="isRegisterLoading" />
-                    </div>
-
-                    <div class="form-group">
-                        <label for="register-api-key" class="form-label">API Key</label>
-                        <input id="register-api-key" type="text" v-model="registerApiKey" class="form-input"
-                            placeholder="请输入你的 Steam API Key" required :disabled="isRegisterLoading" />
-                    </div>
-
-                    <div class="checkbox-container">
-                    </div>
-
-                    <div v-if="registerError" class="error-message">
-                        {{ registerError }}
-                    </div>
-
-                    <div class="button-container">
-                        <button type="submit" class="login-button" :disabled="isRegisterLoading">
-                            <span v-if="isRegisterLoading" class="loading-spinner"></span>
-                            <span>{{ isRegisterLoading ? '验证中...' : '提交' }}</span>
-                        </button>
-                    </div>
-                </form>
-
-                <transition name="toast">
-                    <div v-if="showRegisterSuccess" class="toast-message success">
-                        {{ registerSuccessMessage }}
-                    </div>
-                </transition>
-            </div>
-        </div>
+        </transition>
     </div>
 </template>
 
